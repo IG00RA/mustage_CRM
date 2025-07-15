212aa46422936c6970fb94c9da96953e89bb992f
Ihor Hromadskiy <gromadskiyigor@gmail.com>
2025-07-15 14:42:53 +0300

    [Update] upload sold acc

--- src/components/ModalComponent/UploadSoldAccounts/UploadSoldAccounts.tsx
+++ src/components/ModalComponent/UploadSoldAccounts/UploadSoldAccounts.tsx
@@ -6,16 +6,11 @@ import CancelBtn from '@/components/Buttons/CancelBtn/CancelBtn';
 import SubmitBtn from '@/components/Buttons/SubmitBtn/SubmitBtn';
 import { toast } from 'react-toastify';
 import { useState, useEffect } from 'react';
-import CustomCheckbox from '@/components/Buttons/CustomCheckbox/CustomCheckbox';
-import CustomSelect from '@/components/Buttons/CustomSelect/CustomSelect';
 import CustomDragDropFile from '@/components/Buttons/CustomDragDropFile/CustomDragDropFile';
 import { useTranslations } from 'next-intl';
 import ExcelJS from 'exceljs';
-import { useCategoriesStore } from '@/store/categoriesStore';
 import { ENDPOINTS } from '@/constants/api';
 import { UploadResponse } from '@/components/Pages/UploadSection/UploadSection';
-
-const settingsOptions = ['Upload.modalUpload.check'];
 
 interface UploadSoldAccountsProps {
   setResponseData: (data: UploadResponse) => void;
@@ -30,54 +25,15 @@ export default function UploadSoldAccounts({
 }: UploadSoldAccountsProps) {
   const t = useTranslations('');
 
-  const {
-    categories,
-    subcategoriesWithParams: subcategories,
-    fetchCategories,
-    fetchSubcategories,
-  } = useCategoriesStore();
-
-  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
-  const [selectedSubcategory, setSelectedSubcategory] = useState<string[]>([]);
   const [uploadedFile, setUploadedFile] = useState<File | null>(null);
   const [accountCount, setAccountCount] = useState<number>(0);
   const [file, setFile] = useState<File | null>(null);
-  const [checkedSettings, setCheckedSettings] = useState<
-    Record<string, boolean>
-  >({});
-
-  useEffect(() => {
-    fetchCategories();
-  }, [fetchCategories]);
 
   useEffect(() => {
     if (!file) {
       setAccountCount(0);
     }
   }, [file]);
-
-  useEffect(() => {
-    if (
-      selectedCategory.length > 0 &&
-      selectedCategory[0] !== t('Load.category')
-    ) {
-      const categoryId = categories.find(
-        cat => cat.account_category_name === selectedCategory[0]
-      )?.account_category_id;
-      if (categoryId) {
-        fetchSubcategories(categoryId, false);
-      }
-    } else {
-      setSelectedSubcategory([]);
-    }
-  }, [selectedCategory, categories, fetchSubcategories, t]);
-
-  const toggleCheckbox = (id: string) => {
-    setCheckedSettings(prev => ({
-      ...prev,
-      [id]: !prev[id],
-    }));
-  };
 
   const handleFileUpload = async (file: File) => {
     setUploadedFile(file);
@@ -135,28 +91,8 @@ export default function UploadSoldAccounts({
       return;
     }
 
-    if (
-      selectedSubcategory.length === 0 ||
-      selectedSubcategory[0] === t('Load.names')
-    ) {
-      toast.error(t('Upload.modalUpload.noSubcategory'));
-      return;
-    }
-
-    const selectedSubcategoryObj = subcategories.find(
-      sub => sub.account_subcategory_name === selectedSubcategory[0]
-    );
-
-    if (!selectedSubcategoryObj) {
-      toast.error(t('Upload.modalUpload.noSubcategory'));
-      return;
-    }
-
     const formData = new FormData();
     formData.append('accounts_file', uploadedFile);
-    const subcategoryId =
-      selectedSubcategoryObj.account_subcategory_id.toString();
-    formData.append('subcategory_id', subcategoryId);
 
     try {
       const headers = {
@@ -190,79 +126,21 @@ export default function UploadSoldAccounts({
         setUploadedFile(null);
         setFile(null);
         setAccountCount(0);
-        setSelectedCategory([]);
-        setSelectedSubcategory([]);
       }
     } catch (error) {
       toast.error(`${t('Upload.modalUpload.uploadError')} : ${error}`);
     }
   };
 
-  const handleCategorySelect = (values: string[]) => {
-    const value = values[0] || '';
-    setSelectedCategory([value]);
-    setSelectedSubcategory([]);
-  };
-
-  const handleSubcategorySelect = (values: string[]) => {
-    const value = values[0] || '';
-    setSelectedSubcategory([value]);
-  };
-
   const handleReset = () => {
     setFile(null);
     setUploadedFile(null);
     setAccountCount(0);
-    setSelectedCategory([]);
-    setSelectedSubcategory([]);
-    setCheckedSettings({});
     onClose();
   };
 
   return (
     <form onSubmit={onSubmit} className={styles.form}>
-      <div className={styles.field}>
-        <label className={styles.label}>
-          {t('Upload.modalUpload.category')}
-        </label>
-        <CustomSelect
-          label={t('Load.selectCategory')}
-          options={[
-            t('Load.category'),
-            ...categories.map(cat => cat.account_category_name),
-          ]}
-          selected={selectedCategory}
-          onSelect={handleCategorySelect}
-          multiSelections={false}
-          width={'100%'}
-          // selectWidth={383}
-        />
-      </div>
-
-      <div className={styles.field}>
-        <label className={styles.label}>{t('Upload.modalUpload.names')}</label>
-        <CustomSelect
-          label={t('Load.selectNames')}
-          options={[
-            t('Load.names'),
-            ...subcategories.map(sub => sub.account_subcategory_name),
-          ]}
-          selected={selectedSubcategory}
-          onSelect={handleSubcategorySelect}
-          multiSelections={false}
-          width={'100%'}
-          // selectWidth={383}
-        />
-      </div>
-
-      <div className={styles.field}>
-        <CustomCheckbox
-          checked={checkedSettings[settingsOptions[0]] || false}
-          onChange={() => toggleCheckbox(settingsOptions[0])}
-          label={t(settingsOptions[0])}
-        />
-      </div>
-
       <div className={styles.field}>
         <CustomDragDropFile
           setFile={setFile}
